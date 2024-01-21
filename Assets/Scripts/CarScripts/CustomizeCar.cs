0e00bd4de675ce8205a2422aefed567293fd940c
Anthony <geant@seas.upenn.edu>
2024-01-20 22:35:26 -0500

    fixed
    adjusted to match with time.delta

--- Assets/Scripts/CarScripts/CustomizeCar.cs
+++ Assets/Scripts/CarScripts/CustomizeCar.cs
@@ -42,8 +42,7 @@ public class CustomizeCar : MonoBehaviour
     private IEnumerator LeaveCarSequence() {
         Debug.Log("LeaveCarSequence");
 
-        GameObject AnimatedScene = GameObject.Find("AnimatedScene").gameObject;
-        GameObject Player = AnimatedScene.transform.Find("Player").gameObject;
+        GameObject Player = Car.transform.Find("Player").gameObject;
 
         // set visibility
         Player.SetActive(true);     
@@ -53,10 +52,8 @@ public class CustomizeCar : MonoBehaviour
         float py = Player.transform.position.y;
 
         while(t > 0.0f) {
-            Player.transform.position = new Vector3(Player.transform.position.x + 0.0075f, Player.transform.position.y, Player.transform.position.z);
-            
-            float dy = Mathf.Sin(t * 40.0f) * 0.075f;
-            Player.transform.position = new Vector3(Player.transform.position.x, py + dy, Player.transform.position.z);
+            float dy = Mathf.Sin(t * 40.0f) * 0.875f;
+            Player.transform.Translate(1.4f * Time.deltaTime, dy * Time.deltaTime, 0.0f);
 
             t -= Time.deltaTime;
             yield return null;
@@ -73,14 +70,10 @@ public class CustomizeCar : MonoBehaviour
         Debug.Log("AnimationSequence");
         Debug.Log("Frame: " + Frame);
         Car.GetComponent<RawImage>().texture = carPreview.texture;
-
-        if (Frame != null) {
-            Frame.SetActive(false);
-        }
         
-        float t = 0.50f;
+        float t = 3.00f;
         while(t > 0.0f) {
-            //Frame.transform.position = new Vector3(Frame.transform.position.x, Frame.transform.position.y + 0.03f, Frame.transform.position.z);
+            Frame.transform.Translate(0.0f, 3.0f * Time.deltaTime, 0.0f);
             t -= Time.deltaTime;
             yield return null;
         }
@@ -92,38 +85,44 @@ public class CustomizeCar : MonoBehaviour
         float outSequence = timer - seqLength;
 
         float carY = Car.transform.position.y;
-        float speed = 0.01f;
-        float grassSpeed = 0.05f;
-        float speedBG1 = 0.002f;
-        float speedBG2 = 0.005f;
-        float speedBG3 = 0.007f;
+        float speed = 1.30f;
+        float grassSpeed = 8.55f;
 
         while(timer > 0.0f) {
+            float translateSpeed = speed;
+
             if(timer > outSequence) {
-                Background.transform.position = new Vector3(Background.transform.position.x - speed, Background.transform.position.y, Background.transform.position.z);
-                speed += Time.deltaTime * 0.01f;
+                Background.transform.Translate(-2.0f * translateSpeed * Time.deltaTime, 0.0f, 0.0f);
+                speed += 0.1f * Time.deltaTime;
             }
-            if(timer > outSequence + 2.0f) {
-                Car.transform.position = new Vector3(Car.transform.position.x + speed * 0.7f, Car.transform.position.y, Car.transform.position.z);
+            if(timer > outSequence - 1.0f) {
+                Car.transform.Translate(translateSpeed * Time.deltaTime, 0.0f, 0.0f);
             }
             if(timer < seqLength) {
-                //Background.transform.position = new Vector3(Background.transform.position.x - speed, Background.transform.position.y, Background.transform.position.z);
-                speed -= Time.deltaTime * 0.01f;
+                speed -= 0.1f * Time.deltaTime;
             }
-            if(timer < seqLength - 2.0f) {
-                Car.transform.position = new Vector3(Car.transform.position.x - speed * 0.7f, Car.transform.position.y, Car.transform.position.z);
+            if(timer < seqLength - 4.0f) {
+                translateSpeed *= 0.7f;
+                Car.transform.Translate(-translateSpeed * Time.deltaTime, 0.0f, 0.0f);
+                Chop.transform.Translate(translateSpeed * Time.deltaTime, 0.0f, 0.0f);
             }
 
-            Road.transform.position = new Vector3(Road.transform.position.x - speed, Road.transform.position.y, Road.transform.position.z);
-            Chop.transform.position = new Vector3(Chop.transform.position.x - speed, Chop.transform.position.y, Chop.transform.position.z);
-            Grass.transform.position = new Vector3(Grass.transform.position.x - grassSpeed, Grass.transform.position.y, Grass.transform.position.z);
-            BG1.transform.position = new Vector3(BG1.transform.position.x - speedBG1, BG1.transform.position.y, BG1.transform.position.z);
-            BG2.transform.position = new Vector3(BG2.transform.position.x - speedBG2, BG2.transform.position.y, BG2.transform.position.z);
-            Bell.transform.position = new Vector3(Bell.transform.position.x - speedBG3, Bell.transform.position.y, Bell.transform.position.z);
-            Statue.transform.position = new Vector3(Statue.transform.position.x - speedBG3, Statue.transform.position.y, Statue.transform.position.z);
+            float gSpeedDelta = grassSpeed + speed;
+            
+            float speedBG1 = gSpeedDelta * 0.1f;
+            float speedBG2 = gSpeedDelta * 0.2f;
+            float speedBG3 = gSpeedDelta * 0.5f;
 
-            float dy = Mathf.Sin(timer * 40.0f) * 0.075f;
-            Car.transform.position = new Vector3(Car.transform.position.x, carY + dy, Car.transform.position.z);
+            Road.transform.Translate(-speed * Time.deltaTime, 0.0f, 0.0f);
+            Grass.transform.Translate(-gSpeedDelta * Time.deltaTime, 0.0f, 0.0f);
+            Chop.transform.Translate(-gSpeedDelta * Time.deltaTime, 0.0f, 0.0f);
+            BG1.transform.Translate(-speedBG1 * Time.deltaTime, 0.0f, 0.0f);
+            BG2.transform.Translate(-speedBG2 * Time.deltaTime, 0.0f, 0.0f);
+            Bell.transform.Translate(speedBG3 * Time.deltaTime, 0.0f, 0.0f);
+            Statue.transform.Translate(speedBG3 * Time.deltaTime, 0.0f, 0.0f);
+
+            float dy = Mathf.Sin(timer * 40.0f) * 0.875f;
+            Car.transform.Translate(0.0f, dy * Time.deltaTime, 0.0f);
 
             timer -= Time.deltaTime;
             yield return null;
@@ -154,12 +153,12 @@ public class CustomizeCar : MonoBehaviour
         Car = AnimatedScene.transform.Find("Car").gameObject;
         Background  = AnimatedScene.transform.Find("Background").gameObject;
         Road = AnimatedScene.transform.Find("Road").gameObject;
-        Chop = AnimatedScene.transform.Find("Chop").gameObject;
-        Grass = AnimatedScene.transform.Find("RoadGrass").gameObject;
         BG1 = AnimatedScene.transform.Find("BG1").gameObject;
         BG2 = AnimatedScene.transform.Find("BG2").gameObject;
-        Bell = AnimatedScene.transform.Find("Bell").gameObject;
-        Statue = AnimatedScene.transform.Find("Statue").gameObject;
+        Chop = AnimatedScene.transform.Find("Chop").gameObject;
+        Grass = AnimatedScene.transform.Find("RoadGrass").gameObject;
+        Bell = Grass.transform.Find("Bell").gameObject;
+        Statue = Grass.transform.Find("Statue").gameObject;
     }
 
     void LeaveCarOnClick() {
