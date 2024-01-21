35f1a3edeee2e88df1171214052ed64b4a5ea86a
Raymond Feng <fengray@seas.upenn.edu>
2024-01-21 02:30:59 -0500

    stuff

---
+++ Assets/Scripts/WaitThenAppear.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class WaitThenAppear : MonoBehaviour
+{
+    //public Button btn;
+
+    void Start()
+    {
+        //this.gameObject.SetActive(false);
+        StartCoroutine(wait_then_appear());
+    }
+
+    private IEnumerator wait_then_appear() {
+        Debug.Log("Coroutine started");
+        yield return new WaitForSeconds(2.5f);
+        Debug.Log("Coroutine ended");
+        this.gameObject.SetActive(true);
+        yield return null;
+    }
+}
