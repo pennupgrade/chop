ce3ea7c13fdb6c37c4a4c3f3504205b18a85dcbb
virajdoshi02 <47150131+virajdoshi02@users.noreply.github.com>
2024-04-09 23:36:34 -0400

    Update DragAndDropController.cs

--- Assets/DragAndDropController.cs
+++ Assets/DragAndDropController.cs
@@ -7,9 +7,12 @@ public class DragAndDropController : MonoBehaviour
 {
     public GameObject[] objectsToHide;
     public GameObject[] objectsToShow;
+    public GameObject[] imagesToShow;
+    public GameObject button;
 
     private GameObject spaces;
     private GameObject stickers;
+    private int currentIndex = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -21,23 +24,57 @@ public class DragAndDropController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void Win() {
+    public void Win()
+    {
         //hide stickers & spaces
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 6; i++)
+        {
             spaces.transform.GetChild(i).gameObject.SetActive(false);
             stickers.transform.GetChild(i).gameObject.SetActive(false);
         }
 
-        foreach (GameObject obj in objectsToHide) {
+        foreach (GameObject obj in objectsToHide)
+        {
             obj.SetActive(false);
         }
 
-        foreach (GameObject obj in objectsToShow) {
+        foreach (GameObject obj in objectsToShow)
+        {
             obj.SetActive(true);
         }
-        SceneManager.LoadScene(13);
+
+    }
+
+    public void DragDropDone()
+    {
+        foreach (GameObject obj in objectsToShow)
+        {
+            obj.SetActive(false);
+        }
+        button.SetActive(true);
+        imagesToShow[0].SetActive(true);
+
+    }
+
+    public void NextObject()
+    {
+        // Hide the current object
+        imagesToShow[currentIndex].SetActive(false);
+
+        // Increment the index to show the next object
+        currentIndex = currentIndex + 1;
+
+        if (currentIndex == 4)
+        {
+            SceneManager.LoadScene(13);
+        }
+        else
+        {
+            // Show the next object
+            imagesToShow[currentIndex].SetActive(true);
+        }
     }
 }
